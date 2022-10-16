b9561085f046443beb0c74604b5d65b4df78f188
Andrew Park <andysampark@gmail.com>
2022-10-16 15:16:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./index.css";
+// import video from "./assets";
+import video from "./assets/bgvideo.mp4";
+import logo from "./assets/logo-white.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video autoPlay muted loop id="myVideo">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="w-screen h-screen flex flex-col justify-center items-center pb-52">
+        <img src={logo} alt="logo" className="pb-20" />
+        <h1 className="font-bold text-4xl text-white pb-4">chews</h1>
+        <div className="flex flex-col justify-center items-center text-gray-300 text-md">
+          <h3>chews is currently in beta.</h3>
+          <h3>enter your email to request access to the platform</h3>
+        </div>
+        {/* <input type="text" className="w-1/6 h-7" /> */}
+        <input type="text" className="w-1/6 h-7" />
+      </div>
     </div>
   );
 }
