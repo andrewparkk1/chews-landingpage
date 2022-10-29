5f74ddad8c3bc30f76a47b327918610e2ca3a7ca
Andrew Park <andysampark@gmail.com>
2022-10-29 13:23:59 -0700

    done

--- src/App.js
+++ src/App.js
@@ -41,36 +41,36 @@ function App() {
       >
         <source src={bgvideo} type="video/mp4" />
       </video>
-      <div className="fadein">
-        <div className=" w-screen h-screen z-[-1] fixed object-cover top-0 bottom-0 right-0 left-0">
-          <div className="w-screen h-screen flex flex-col justify-center items-center pb-52">
-            <img src={logo} alt="logo" className="pb-20" />
-            <h1 className="font-bold text-6xl text-white pb-4">chews</h1>
-            <h2 className="font-bold text-3xl text-white pb-4 text-center px-10">
-              simplifying the search for food
-            </h2>
+      <div className="fadein w-screen h-screen flex flex-col justify-center items-center">
+        <div className="w-screen h-screen flex flex-col justify-center items-center flex-grow pb-20">
+          <img src={logo} alt="logo" className="pb-20" />
+          <h1 className="font-bold text-6xl text-white pb-4">chews</h1>
+          <h2 className="font-bold text-3xl text-white pb-4 text-center px-10">
+            simplifying the search for food
+          </h2>
 
-            <div className="flex flex-col justify-center items-center text-gray-200 text-md pt-20 font-semibold">
-              <h3>Sign up for updates</h3>
-            </div>
+          <div className="flex flex-col justify-center items-center text-gray-200 text-md pt-20 font-semibold">
+            <h3>Sign up for updates</h3>
+          </div>
 
-            <form onSubmit={handleSubmit} className="py-4">
-              <input
-                type="email"
-                name="email"
-                placeholder="your@email.com"
-                className="required email p-2 rounded-md border-2 border-white"
-                onChange={(e) => setData({ [e.target.name]: e.target.value })}
-              />
-              <button
-                className="button cursor-pointer text-white border-white border-2 py-2 px-6 bg-[rgb(69, 182, 237)] rounded-md font-semibold"
-                type="submit"
-              >
-                send
-              </button>
-            </form>
-            <p className="text-white font-semibold">{update}</p>
-            {/* <MailchimpSubscribe
+          <form onSubmit={handleSubmit} className="py-4">
+            <input
+              type="email"
+              name="email"
+              placeholder="your@email.com"
+              className="required email p-2 rounded-md border-2 border-white"
+              onChange={(e) => setData({ [e.target.name]: e.target.value })}
+            />
+            <button
+              className="button cursor-pointer text-white border-white border-2 py-2 px-6 bg-[rgb(69, 182, 237)] rounded-md font-semibold"
+              type="submit"
+            >
+              send
+            </button>
+          </form>
+          <p className="text-white font-semibold">{update}</p>
+        </div>
+        {/* <MailchimpSubscribe
               url={url}
               render={({ subscribe, status, message }) => (
                 <CustomForm
@@ -81,7 +81,7 @@ function App() {
               )}
             /> */}
 
-            {/* <div className="flex flex-col items-center justify-center text-gray-200">
+        {/* <div className="flex flex-col items-center justify-center text-gray-200">
               <div className="flex flex-row gap-5">
                 <a
                   rel="noreferrer"
@@ -112,38 +112,36 @@ function App() {
               </div>
             </div> */}
 
-            <div class="mx-auto flex flex-col space-y-2 py-4 text-center text-gray-200 absolute bottom-1">
-              <div className="flex flex-row gap-5 pb-2">
-                <a
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://www.instagram.com/chewscorp/"
-                  className="font-semibold"
-                >
-                  instagram
-                </a>
-                <p>|</p>
-                <a
-                  rel="noreferrer"
-                  href="https://twitter.com/chews_chews"
-                  target="_blank"
-                  className="font-semibold"
-                >
-                  twitter
-                </a>
-                <p>|</p>
-                <a
-                  rel="noreferrer"
-                  href="https://www.tiktok.com/@chews.wisely?_t=8WqUVGopPEU&_r=1"
-                  target="_blank"
-                  className="font-semibold"
-                >
-                  tiktok
-                </a>
-              </div>
-              <p class="text-xs">© Chews Inc. All Rights Reserved.</p>
-            </div>
+        <div class="mx-auto flex flex-col space-y-2 py-4 text-center text-gray-200">
+          <div className="flex flex-row gap-5 pb-2">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.instagram.com/chewscorp/"
+              className="font-semibold"
+            >
+              instagram
+            </a>
+            <p>|</p>
+            <a
+              rel="noreferrer"
+              href="https://twitter.com/chews_chews"
+              target="_blank"
+              className="font-semibold"
+            >
+              twitter
+            </a>
+            <p>|</p>
+            <a
+              rel="noreferrer"
+              href="https://www.tiktok.com/@chews.wisely?_t=8WqUVGopPEU&_r=1"
+              target="_blank"
+              className="font-semibold"
+            >
+              tiktok
+            </a>
           </div>
+          <p class="text-xs">© Chews Inc. All Rights Reserved.</p>
         </div>
       </div>
     </div>
